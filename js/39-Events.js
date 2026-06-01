041f2d862878635bd53e075b2910922c2e7e6811
Docent <docent@cetpenedes.cat>
2026-06-01 13:50:10 +0200

    Sessió 01/06

---
+++ js/39-Events.js
@@ -0,0 +1,29 @@
+function fesClic() {
+  alert("Hem fet clic sobre el cotó");
+}
+
+function fesDobleClic() {
+  alert("Hem fet doble clic sobre el cotó");
+}
+
+function canviaColor() {
+  document.getElementById('caixa').style.backgroundColor="red";
+}
+
+function tornaColor() {
+  document.getElementById('caixa').style.backgroundColor="white";
+}
+
+function mostraNumero(){
+    console.log(852);
+    document.getElementById('caixa').style.backgroundColor="green";
+}
+
+function canviaOnOff(){
+    alert("He apretat el botó del details/summary");
+    console.log("He apretat el botó de details/summary");
+}
+
+function msgBenvinguda(){
+    alert("Bon dia company!")
+}
