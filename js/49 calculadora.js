b456b0d5fa0b12d62bb3bb45041158e80852c42d
Docent <docent@cetpenedes.cat>
2026-06-25 13:56:40 +0200

    Sessió 25/06

--- js/49 calculadora.js
+++ js/49 calculadora.js
@@ -1,19 +1,16 @@
-function afegir(caracter){
-    let cadena = sessionStorage.getItem("resultat");
-    if(isFinite(caracter) || isFinite(cadena[cadena.length-1])){
-        cadena+=caracter;
-        }
-        else{
-            cadena=cadena.substring(0,cadena.length-1);
-            cadena+=caracter;
+let cadena = "";
 
-        //cadena[cadena.length-1]=caracter;
-    }
-    sessionStorage.setItem("resultat",cadena);
-    document.getElementById("display").value=cadena;
+function afegir(caracter) {
+  if (isFinite(caracter) || isFinite(cadena[cadena.length - 1])) {
+    cadena += caracter;
+  } else {
+    cadena = cadena.substring(0, cadena.length - 1);
+    cadena += caracter;
+  }
+  document.getElementById("display").value = cadena;
 }
 
-// function resol(){
+// function calcular(){
 //     try{
 //         let cadena = sessionStorage.getItem("resultat");
 //     document.getElementById("display").value=eval(cadena);
@@ -24,48 +21,45 @@ function afegir(caracter){
 
 alerta = (missatge) => alert(missatge);
 
-resol = () => {
-    try{
-        let cadena = sessionStorage.getItem("resultat");
-        document.getElementById("display").value=eval(cadena);
-    }catch(e){
-        //alert("Alguna cosa no ha anat bé");
-        alerta("Hola bon dia");
-    }
+calcular = () => {
+  try {
+    document.getElementById("display").value = eval(cadena);
+  } catch (e) {
+    //alert("Alguna cosa no ha anat bé");
+    alerta("Hola bon dia");
+  }
+};
+
+function netejar(opcio) {
+  switch (opcio) {
+    case "C":
+      esborraTot();
+      break;
+    case "CE":
+      //console.log("CE");
+      esborraOperador();
+      break;
+    case "<":
+      //console.log("<");
+      esborraCaracter();
+      break;
+  }
 }
 
-
-function netejar(opcio){
-    switch(opcio){
-        case 'C':
-            //console.log("C");
-            esborraTot();
-            break;
-            case 'CE':
-            //console.log("CE");
-            esborraOperador();
-            break;
-            case '<':
-            //console.log("<");
-            esborraCaracter();
-            break;
-    }
+function esborraTot() {
+    console.log("Valor de la cadena abans de esborrar: "+cadena);
+  cadena = "";
+  console.log("Valor de la cadena després de esborrar: "+cadena);
+  document.getElementById("display").value = "";
 }
 
-function esborraTot(){
-    sessionStorage.setItem("resultat","");
-    document.getElementById("display").value="";
-}
-
-function esborraCaracter(){
-    let cadena = sessionStorage.getItem("resultat");
-    //console.log("Longitud:"+cadena.length);
-    //console.log(cadena);
-    if(cadena.length>0){
-        sessionStorage.setItem("resultat",cadena.substring(0,cadena.length-1));
-        document.getElementById("display").value=cadena;
-    }
-    else{
-        alert("No hi ha més digits per esborrar!");
-    }
+function esborraCaracter() {
+  let cadena = sessionStorage.getItem("resultat");
+  console.log("valor de la cadena abans del if: "+cadena);
+  if (cadena.length > 0) {
+    cadena=cadena.substring(0, cadena.length - 1);
+    document.getElementById("display").value = cadena;
+  } else {
+    alert("No hi ha més digits per esborrar!");
+  }
 }
