bad1f690de54e737bcf88cce4b3c00457b8c99fd
Enric <enricg@protonmail.com>
2026-07-05 23:05:52 +0200

    Afegir then

--- js/56 JSON.js
+++ js/56 JSON.js
@@ -9,11 +9,15 @@
 // const obj = JSON.parse(text);
 // console.log(obj);
 
-// document.getElementById("taula").addEventListener("load", carregaDades);
+window.addEventListener("load", function () {
+  // carregaDades();
+  carregaDades2();
+});
 
 const taula = document.getElementById("taula");
 let capcalera = [];
 
+// Versió amb async/await
 async function carregaDades() {
   try {
     const resposta = await fetch("./JSON/2024_pad_imm_mdbas.json");
@@ -34,9 +38,31 @@ async function carregaDades() {
   }
 }
 
-function afegeixRegistre(element,etiqueta="td") {
+// Versió amb promeses
+function carregaDades2() {
+  fetch("./JSON/2024_pad_imm_mdbas.json")
+    .then((resposta) => resposta.json())
+    .then((persones) => {
+      //recorrer el camps per a la capçalera
+      Object.keys(persones[0]).forEach((clau) => {
+        const camp = document.createElement("th");
+        camp.innerText = clau;
+        taula.appendChild(camp);
+      });
+
+      //recorrem els elements del json
+      persones.forEach((element) => {
+        taula.appendChild(afegeixRegistre(element));
+      });
+    })
+    .catch((e) => {
+      console.error("Error de càrrega ", e);
+    });
+}
+
+function afegeixRegistre(element, etiqueta = "td") {
   const registre = document.createElement("tr");
-  
+
   Object.keys(element).forEach((clau) => {
     const camp = document.createElement(etiqueta);
     camp.innerText = element[clau];
