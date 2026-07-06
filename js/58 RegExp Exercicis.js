aad401098765a2edc71cc575ae6fc67e912b7d0b
Enric <enricg@protonmail.com>
2026-07-07 00:17:55 +0200

    Generacío automàtica d'enunciats d'exercicis

--- js/58 RegExp Exercicis.js
+++ js/58 RegExp Exercicis.js
@@ -1,55 +1,83 @@
-// Funció per validar exercici
-function exerciciBool(pSolucio, pText, pPatro, pMissatgeOK, pMissatgeNOK) {
-  const solucio = document.getElementById(pSolucio);
-  let text = document.getElementById(pText).value;
+// Creem els exercicis amb el títol i l'enunciat
+function creaExercicis() {
+  let comptador = 1;
 
-  let textpatro = pPatro;
-  const patro = new RegExp(textpatro);
-  let resultat = text.match(patro);
+  // Recorrem l'array d'exercicis i creem els elements HTML corresponents
+  exercicis.forEach((exercici) => {
+    const nouExercici = document.createElement("div");
+    nouExercici.classList.add("exercici");
+
+    const titol = document.createElement("div");
+    titol.classList.add("titol");
+    titol.textContent = comptador + ". " + exercici.titol;
+    nouExercici.appendChild(titol);
+
+    const enunciat = document.createElement("div");
+    enunciat.classList.add("enunciat");
+    enunciat.textContent = exercici.enunciat;
+    nouExercici.appendChild(enunciat);
+
+    const entrada = document.createElement("input");
+    entrada.type = "text";
+    entrada.id = "txt" + comptador;
+    nouExercici.appendChild(entrada);
+
+    const boto = document.createElement("button");
+    boto.id = "btn" + comptador;
+    boto.textContent = "Executa";
+    nouExercici.appendChild(boto);
+
+    const solucio = document.createElement("div");
+    solucio.id = "sol" + comptador;
+    solucio.classList.add("missatgeOK");
+    nouExercici.appendChild(solucio);
+
+    document.getElementById("principal").appendChild(nouExercici);
+    comptador++; // Incrementem el comptador per al següent exercici
+  });
+}
+
+// Funció per validar exercici
+function exercici(numExercici, txtPatro) {
+  const solucio = document.getElementById("sol" + numExercici);
+  let text = document.getElementById("txt" + numExercici).value;
+  let resultat = text.match(txtPatro);
+
+  switch (exercicis[numExercici - 1].tipusResposta) {
+    case "boolea":
+      resultat = txtPatro.test(text);
+      break;
+    case "llista":
+      resultat = text.match(txtPatro);
+      break;
+    case "cadena":
+      resultat = resultat ? resultat[0] : null;
+      break;
+    default:
+      resultat = null;
+  }
 
   if (resultat) {
-    solucio.innerText = missatgeOK;
     solucio.classList.replace("missatgeNOK", "missatgeOK");
+    solucio.innerText = resultat;
   } else {
-    solucio.innerText = missatgeNOK;
     solucio.classList.replace("missatgeOK", "missatgeNOK");
+    solucio.innerText = false;
   }
 }
 
-function exerciciValors(pSolucio, pText, pPatro) {
-  const solucio = document.getElementById(pSolucio);
-  let text = document.getElementById(pText).value;
-
-  let textpatro = pPatro;
-  const patro = new RegExp(textpatro);
-  let resultat = text.match(patro);
-
-  solucio.innerText = resultat;
-}
-
-creaElement(titol,enunciat){
-    // const exercici= document.getElementById("principal");
-    const nouExercici=document.createElement("div");
-    nouExercici.classList.add("exercici");
-    const titol=document.createElement("div");
-    titol.classList.add("titol");
-    titol.textContent=titol;
-    const enunciat=document.createElement("div");
-    enunciat.classList.add("enunciat");
-    enunciat.textContent=enunciat;
-    // const entrada=document.createElement("input");
-    // const executa=document.createElement("button");
-    // const solucio=document.createElement("div");
-    nouExercici.appendChild(titol);
-    nouExercici.appendChild(enunciat);
-}
+// Listeners
+// Esperem que el document estigui carregat abans de crear les etiquetes dels exercicis
+document
+  .getElementById("principal")
+  .addEventListener("DOMContentLoaded", creaExercicis());
 
 // Exercici 1
 document.getElementById("btn1").addEventListener("click", function () {
-  exerciciBool("sol1","txt1","\\w+@\\w+\\.\\w{2,6}","email vàlid","email NO vàlid");
+  exercici(1, /\w+@\w+\.\w{2,6}/);
 });
 
 // Exercici 2
 document.getElementById("btn2").addEventListener("click", function () {
-  exerciciValors("sol2", "txt2", "(\\d{3})\\d{3}-\\d{4}");
+  exercici(2, /\(\d{3}\)\d{3}-\d{4}/g);
 });
