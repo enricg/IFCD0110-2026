8f1a8e2e28bc32fa59d5de2b7b2d08b966d8ec9d
Enric <enricg@protonmail.com>
2026-07-09 23:18:24 +0200

    Modificació total multiidioma

--- js/60 MultiIdioma.js
+++ js/60 MultiIdioma.js
@@ -1,24 +1,61 @@
-const capçalera = document.getElementById("titol");
+let dadesIdiomes = null;
 
-document.addEventListener(onload, carregaIdioma(navigator.language));
+// Carrega el fitxer JSON amb les dades dels idiomes
+async function carregaJSON() {
+  try {
+    const resposta = await fetch("./JSON/60 MultiIdioma Dades.json");
 
-function tradueix(index) {
-  const titol = document.getElementById("titol");
-  titol.innerHTML = idiomes[index].titol;
+    if (!resposta.ok) {
+      throw new Error("No s'ha pogut carregar el fitxer JSON.");
+    }
+
+    dadesIdiomes = await resposta.json();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function carregaIdioma(idioma) {
-  switch (idioma) {
-    case "ca-CA":
-      tradueix(0);
-      break;
-    case "es-ES":
-      tradueix(1);
-      break;
-    case "en-US":
-      tradueix(2);
-      break;
-    default:
-      tradueix(0);
+// Canvia l'idioma de la pàgina segons l'idioma seleccionat
+function canviarIdioma(idioma) {
+  const dades = dadesIdiomes.idiomes.find((item) => item.idioma === idioma);
+
+  if (!dades) return;
+
+  document.getElementById("titol").innerHTML = dades.titol;
+
+  for (let i = 0; i < dades.menu.length; i++) {
+    document.getElementById("menu" + i).innerHTML = dades.menu[i];
   }
+
+  document.getElementById("peu").innerHTML = dades.peu;
+}
+
+// Espera a que la pàgina estigui carregada abans de carregar el fitxer JSON i establir l'idioma inicial
+window.onload = async function () {
+  await carregaJSON();
+  canviarIdioma("ca"); // català inicial
+};
+
+const seleccionaIdioma = document.getElementById("idiomes");
+// Afegim un event listener per canviar l'idioma quan l'usuari selecciona un idioma diferent
+idiomes.addEventListener("change", function () {
+  const idiomaSeleccionat = this.value;
+  canviarIdioma(idiomaSeleccionat);
+});
+
+// Funció per canviar l'idioma de la pàgina
+function canviarIdioma(idioma) {
+  const dades = dadesIdiomes.idiomes.find((item) => item.idioma === idioma);
+
+  if (!dades) return;
+
+  document.getElementById("titol").innerHTML = dades.titol;
+
+  const menu = document.querySelectorAll("li");
+
+  menu.forEach((element, index) => {
+    element.innerHTML = dades.menu[index];
+  });
+
+  document.getElementById("peu").innerHTML = dades.peu;
 }
