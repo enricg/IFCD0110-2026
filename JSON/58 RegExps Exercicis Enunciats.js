6769f2a6e6ab6f428a387459702873531431154e
Enric <enricg@protonmail.com>
2026-07-09 14:50:30 +0200

    Afegit bootstrap i respostes

--- JSON/58 RegExps Exercicis Enunciats.js
+++ JSON/58 RegExps Exercicis Enunciats.js
@@ -2,161 +2,173 @@ const exercicis = [
   {
     titol: "Validació de correus electrònics",
     enunciat:
-      "Escriu una funció esEmailValid(email) que retorni true si l'email és vàlid i false altrament.",
+      "Escriu una funció <b>esEmailValid(email)</b> que retorni true si l'email és vàlid i false altrament.",
     tipusResposta: "boolea",
-    pista: "Ha de contenir **@**, i després un **.**",
+    pista: "Ha de contenir <b>@</b>, i després un <b>.</b>",
     proves: "test@example.com // true<br>test.example.com // false",
   },
   {
     titol: "Extracció de números de telèfon",
     enunciat:
-      "Escriu una funció extreureNombresTelefon(text) que retorni una llista de números de telèfon trobats en el text.",
+      "Escriu una funció <b>extreureNombresTelefon(text)</b> que retorni una llista de números de telèfon trobats en el text.",
     tipusResposta: "llista",
-    pista: "Ha de contenir **3 números entre parèntesis**, després **3 números**, un **-** i **4 números** més",
-    proves: "",
+    pista:
+      "Ha de contenir <b>3 números entre parèntesis</b>, després <b>3 números</b>, un <b>-</b> i <b>4 números</b> més",
+    proves:
+      "Call me at (123) 456-7890 or (987) 654-3210, // ['(123) 456-7890', '(987) 654-3210']",
   },
   {
     titol: "Validació de codis postals",
     enunciat:
-      "Escriu una funció esCpValid(zip) que retorni true si el codi postal és vàlid i false altrament.",
+      "Escriu una funció <b>esCpValid(zip)</b> que retorni true si el codi postal és vàlid i false altrament.",
     tipusResposta: "boolea",
-    pista: "",
-    proves: "",
+    pista: "Ha de contenir 5 números",
+    proves: "12345 // true<br>12345-6789 // true<br>123456 // false",
   },
   {
     titol: "Extracció de dominis de correu electrònic",
     enunciat:
-      "Escriu una funció extraureDominiEmail(email) que retorni el domini de l'email.",
+      "Escriu una funció <b>extraureDominiEmail(email)</b> que retorni el domini de l'email.",
     tipusResposta: "cadena",
-    pista: "",
-    proves: "",
+    pista: "S'ha de treure tot el què hi hagi després de la <b>@</b>",
+    proves: "test@example.com; // example.com",
   },
   {
     titol: "Validació de números de targeta de crèdit",
     enunciat:
-      "Escriu una funció esTargetaCreditValida(number) que retorni true si el número de targeta és vàlid i,false altrament.",
+      "Escriu una funció <b>esTargetaCreditValida(number)</b> que retorni true si el número de targeta és vàlid i false altrament.",
     tipusResposta: "boolea",
-    pista: "",
-    proves: "",
+    pista:
+      "Ha de contenir <b>16 números</b>, <b>tots junts</b>, o <b>separats per espais en grups de 4</b>",
+    proves:
+      "1234 5678 9012 3456 // true<br>1234567890123456 // true<br>1234-5678-9012-3456 // false",
   },
   {
     titol: "Extracció de hashtags",
     enunciat:
-      "Escriu una funció extraureHashtags(text) que retorni una llista de hashtags trobats en el text.",
+      "Escriu una funció <b>extraureHashtags(text)</b> que retorni una llista de hashtags trobats en el text.",
     tipusResposta: "llista",
-    pista: "",
-    proves: "",
+    pista: "Extraure el <b>#</b> i la <b>paraula adjunta</b>",
+    proves:
+      "Loving the new #JavaScript features in #ECMAScript2021! // [#JavaScript, #ECMAScript2021]",
   },
   {
     titol: "Validació de contrasenyes",
     enunciat:
-      "Escriu una funció esPasswordValida(password) que retorni true si la contrasenya és vàlida i false altrament.",
+      "Escriu una funció <b>esPasswordValida(password)</b> que retorni true si la contrasenya és vàlida i false altrament.",
     tipusResposta: "boolea",
-    pista: "",
-    proves: "",
+    pista:
+      "Ha de contenir <b>lletres</b>, <b>números</b>, i <b>caràcters especials</b>",
+    proves:
+      "Password1! // true<br>password1 // false<br>Password // false<br>Password! // false<br>Password1 // false",
   },
   {
     titol: "Extracció de dates",
     enunciat:
-      "Escriu una funció extraureDates(text) que retorni una llista de dates trobades en el text.",
+      "Escriu una funció <b>extraureDates(text)</b> que retorni una llista de dates trobades en el text.",
     tipusResposta: "llista",
     pista: "",
-    proves: "",
+    proves:
+      "My birthday is on 12/05/1992 and my brother's is on 03/10/1995. // [12/05/1992, 03/10/1995]",
   },
   {
     titol: "Eliminació de duplicats en una cadena",
     enunciat:
-      "Escriu una funció treureParaulesDuplicades(text) que retorni la cadena sense paraules duplicades,consecutives.",
+      "Escriu una funció <b>treureParaulesDuplicades(text)</b> que retorni la cadena sense paraules duplicades,consecutives.",
     tipusResposta: "cadena",
-    pista: "",
-    proves: "",
+    pista: "Retorn text.replaceregex('$1')",
+    proves: "This is is a test test. // This is a test.",
   },
   {
     titol: "Validació de URL",
     enunciat:
-      "Escriu una funció esURLValida(url) que retorni true si la URL és vàlida i false altrament.",
+      "Escriu una funció <b>esURLValida(url)</b> que retorni true si la URL és vàlida i false altrament.",
     tipusResposta: "boolea",
-    pista: "",
-    proves: "",
+    pista: "return regex.test(url)",
+    proves:
+      "https://www.example.com // true<br>ftp://files.example.com // true<br>htp://invalid-url.com // false",
   },
   {
     titol: "Validació de números de seguretat social",
     enunciat:
-      "Escriu una funció esSegSocValida(ssn) que retorni true si el número de seguretat social és vàlid i false altrament.",
+      "Escriu una funció <b>esSegSocValida(ssn)</b> que retorni true si el número de seguretat social és vàlid i false altrament.",
     tipusResposta: "boolea",
-    pista: "",
-    proves: "",
+    pista: "9 números separats en tres grups de 3 per guions",
+    proves: "123-45-6789 // true<br>123-456-789 // false",
   },
   {
     titol: "Extracció de noms d'usuari de Twitter",
     enunciat:
-      "Escriu una funció extraureUsuarisTwitter(text) que retorni una llista de noms d'usuari de Twitter trobats en el text.",
+      "Escriu una funció <b>extraureUsuarisTwitter(text)</b> que retorni una llista de noms d'usuari de Twitter trobats en el text.",
     tipusResposta: "llista",
-    pista: "",
-    proves: "",
+    pista: "<b>@</b> seguida de nom d'usuari",
+    proves:
+      "Follow us on Twitter @example and @test_account. // [@example, @test_account]",
   },
   {
     titol: "Validació de codis de barres EAN-13",
     enunciat:
-      "Escriu una funció esEAN13Valid(code) que retorni true si el codi de barres EAN-13 és vàlid i false altrament.",
+      "Escriu una funció <b>esEAN13Valid(code)</b> que retorni true si el codi de barres EAN-13 és vàlid i false altrament.",
     tipusResposta: "boolea",
     pista: "",
-    proves: "",
+    proves: "4006381333931 // true<br>123456789012 // false",
   },
   {
     titol: "Extracció de URLs",
     enunciat:
-      "Escriu una funció extractURLs(text) que retorni una llista de URLs trobades en el text.",
+      "Escriu una funció <b>extractURLs(text)</b> que retorni una llista de URLs trobades en el text.",
     tipusResposta: "llista",
     pista: "",
-    proves: "",
+    proves:
+      "Visit our site at https://www.example.com or http://www.test.com // [https://www.example.com, http://www.test.com]",
   },
   {
     titol: "Validació de noms de fitxers",
     enunciat:
-      "Escriu una funció esFitxerValid(fileName) que retorni true si el nom de fitxer és vàlid i false altrament.",
+      "Escriu una funció <b>esFitxerValid(fileName)</b> que retorni true si el nom de fitxer és vàlid i false altrament.",
     tipusResposta: "boolea",
     pista: "",
-    proves: "",
+    proves: "example.txt // true<br>example.docx // false",
   },
   {
     titol: "Extracció de codis HEX de colors",
     enunciat:
-      "Escriu una funció extraureColorsHex(text) que retorni una llista de codis HEX de colors trobats en el text.",
+      "Escriu una funció <b>extraureColorsHex(text)</b> que retorni una llista de codis HEX de colors trobats en el text.",
     tipusResposta: "llista",
     pista: "",
-    proves: "",
+    proves: "Colors: #FF5733, #FFF, and #123456. // [#FF5733, #FFF, #123456]",
   },
   {
     titol: "Validació de números ISBN-10",
     enunciat:
-      "Escriu una funció esISBN10Valid(isbn) que retorni true si el número ISBN-10 és vàlid i false altrament.",
+      "Escriu una funció <b>esISBN10Valid(isbn)</b> que retorni true si el número ISBN-10 és vàlid i false altrament.",
     tipusResposta: "boolea",
     pista: "",
-    proves: "",
+    proves: "0306406152 // true<br>123456789X // true<br>1234567890 // false",
   },
   {
     titol: "Extracció de paraules amb lletra majúscula",
     enunciat:
-      "Escriu una funció extraureParaulesMajuscula(text) que retorni una llista de paraules amb lletra majúscula trobades en el text.",
+      "Escriu una funció <b>extraureParaulesMajuscula(text)</b> que retorni una llista de paraules amb lletra majúscula trobades en el text.",
     tipusResposta: "llista",
     pista: "",
-    proves: "",
+    proves: "Hello World, this is JavaScript! // [Hello, World, JavaScript]",
   },
   {
     titol: "Validació de plaquetes de matrícula",
     enunciat:
-      "Escriu una funció esMatriculaValida(plate) que retorni true si la plaqueta de matrícula és vàlida i false altrament.",
+      "Escriu una funció <b>esMatriculaValida(plate)</b> que retorni true si la plaqueta de matrícula és vàlida i false altrament.",
     tipusResposta: "boolea",
     pista: "",
-    proves: "",
+    proves: "ABC-123 // true<br>AB-1234 // false",
   },
   {
     titol: "Extracció de preus",
     enunciat:
-      "Escriu una funció extraurePreus(text) que retorni una llista de preus trobats en el text.",
+      "Escriu una funció <b>extraurePreus(text)</b> que retorni una llista de preus trobats en el text.",
     tipusResposta: "llista",
     pista: "",
-    proves: "",
+    proves:
+      "The total cost is $15.99, but with discount it's only $12.00. // [$15.99, $12.00]",
   },
 ];
--- js/58 RegExp Exercicis.js
+++ js/58 RegExp Exercicis.js
@@ -4,34 +4,81 @@ function creaExercicis() {
 
   // Recorrem l'array d'exercicis i creem els elements HTML corresponents
   exercicis.forEach((exercici) => {
+    // Creem un div per a cada exercici i li afegim la classe "exercici"
     const nouExercici = document.createElement("div");
     nouExercici.classList.add("exercici");
 
+    // Creem el títol i l'afegim a l'exercici
     const titol = document.createElement("div");
     titol.classList.add("titol");
     titol.textContent = comptador + ". " + exercici.titol;
     nouExercici.appendChild(titol);
 
+    // Creem l'enunciat i l'afegim a l'exercici
     const enunciat = document.createElement("div");
     enunciat.classList.add("enunciat");
-    enunciat.textContent = exercici.enunciat;
+    enunciat.innerHTML = exercici.enunciat;
     nouExercici.appendChild(enunciat);
 
+    // Creem l'input de text i l'afegim a l'exercici
     const entrada = document.createElement("input");
+    entrada.classList.add("form-control");
     entrada.type = "text";
     entrada.id = "txt" + comptador;
     nouExercici.appendChild(entrada);
 
+    // Creem un paràgraf per mostrar missatges addicionals i l'afegim a l'exercici
+    const extra = document.createElement("p");
+    extra.classList.add("d-inline-flex");
+    extra.classList.add("gap-1");
+    nouExercici.appendChild(extra);
+
+    // Creem el botó d'execució i l'afegim a l'exercici
     const boto = document.createElement("button");
     boto.id = "btn" + comptador;
+    boto.classList.add("btn");
+    boto.classList.add("btn-primary");
     boto.textContent = "Executa";
-    nouExercici.appendChild(boto);
+    extra.appendChild(boto);
 
+    // Creem un botó addicional dins del paràgraf per a funcionalitats extra i l'afegim al paràgraf
+    const botoExtra = document.createElement("button");
+    botoExtra.classList.add("btn");
+    botoExtra.classList.add("btn-primary");
+    botoExtra.setAttribute("data-bs-toggle", "collapse");
+    botoExtra.setAttribute("data-bs-target", "#collapseExample" + comptador);
+    botoExtra.setAttribute("aria-expanded", "false");
+    botoExtra.setAttribute("aria-controls", "collapseExample");
+    botoExtra.textContent = "+info";
+    extra.appendChild(botoExtra);
+
+    const textExtra = document.createElement("div");
+    textExtra.id = "collapseExample" + comptador;
+    textExtra.classList.add("collapse");
+    nouExercici.appendChild(textExtra);
+
+    const tarjetaExtra = document.createElement("div");
+    // tarjetaExtra.classList.add("card");
+    // tarjetaExtra.classList.add("card-body");
+    
+    // Afegim la pista i les proves (si n'hia ha) dins del div de text addicional
+    let pista = exercicis[comptador - 1].pista
+      ? "Pista:<br>" + exercicis[comptador - 1].pista
+      : "";
+    let proves = exercicis[comptador - 1].proves
+      ? "<br>Proves:<br>" + exercicis[comptador - 1].proves
+      : "";
+    tarjetaExtra.innerHTML = pista + proves;
+    textExtra.appendChild(tarjetaExtra);
+
+    // Creem el div per mostrar la solució i l'afegim a l'exercici
     const solucio = document.createElement("div");
     solucio.id = "sol" + comptador;
-    solucio.classList.add("missatgeOK");
+    solucio.classList.add("alert");
+    solucio.classList.add("alert-light");
     nouExercici.appendChild(solucio);
 
+    // Afegim l'exercici complet al div principal
     document.getElementById("principal").appendChild(nouExercici);
     comptador++; // Incrementem el comptador per al següent exercici
   });
@@ -49,7 +96,7 @@ function exercici(numExercici, txtPatro) {
     case "llista":
       resultat = text.match(txtPatro);
       break;
-      case "cadena":
+    case "cadena":
       resultat = text.match(txtPatro);
       resultat = resultat ? resultat[0] : null;
       break;
@@ -58,10 +105,10 @@ function exercici(numExercici, txtPatro) {
   }
 
   if (resultat) {
-    solucio.classList.replace("missatgeNOK", "missatgeOK");
+    solucio.classList.replace("alert-danger", "alert-light");
     solucio.innerText = resultat;
   } else {
-    solucio.classList.replace("missatgeOK", "missatgeNOK");
+    solucio.classList.replace("alert-light", "alert-danger");
     solucio.innerText = false;
   }
 }
@@ -80,7 +127,6 @@ document.getElementById("btn1").addEventListener("click", function () {
 });
 
 // Exercici 2
-// Call me at (123) 456-7890 or (987) 654-3210 // ["(123) 456-7890", "(987) 654-3210"]
 document.getElementById("btn2").addEventListener("click", function () {
   exercici(2, /\(\d{3}\)\s?\d{3}-\d{4}/g);
 });
@@ -91,22 +137,32 @@ document.getElementById("btn3").addEventListener("click", function () {
 });
 
 // Exercici 4
-//test@example.com // "example.com"
 document.getElementById("btn4").addEventListener("click", function () {
   exercici(4, /(?<=\w+@)\w+\.\w+/);
 });
 
 // Exercici 5
-// 1234 5678 9012 3456 // true
-// 1234567890123456 // true
-// 1234-5678-9012-3456 // false
 document.getElementById("btn5").addEventListener("click", function () {
   exercici(5, /^(\d{16}|\d{4}\s\d{4}\s\d{4}\s\d{4})$/);
 });
 
 // Exercici 6
-//Loving the new #JavaScript features in #ECMAScript2021! // ["#JavaScript", "#ECMAScript2021"]
 document.getElementById("btn6").addEventListener("click", function () {
   exercici(6, /#\w+/g);
+});
+
+// Exercici 7
+document.getElementById("btn7").addEventListener("click", function () {
+  exercici(7, /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/);
+});
+
+// Exercici 8
+document.getElementById("btn8").addEventListener("click", function () {
+  exercici(8, /^[\w,\s-]+\.[A-Za-z]{3}$/);
+});
+
+// Exercici 9
+document.getElementById("btn9").addEventListener("click", function () {
+  exercici(9, /#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\b/g);
 });
 
