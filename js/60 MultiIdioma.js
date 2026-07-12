788b3113e2e6406f7b6a30b92dd9ebc9511184ef
Enric <enricg@protonmail.com>
2026-07-12 22:17:08 +0200

    add imatges pokemons

--- js/60 MultiIdioma.js
+++ js/60 MultiIdioma.js
@@ -6,7 +6,7 @@ let dadesIdiomes = null;
 // Espera a que la pàgina estigui carregada abans de carregar el fitxer JSON i establir l'idioma inicial
 window.onload = async function () {
   await carregaJSON();
-  canviarIdioma(navigator.language); // català inicial
+  canviarIdioma(navigator.language.substring(0,2)); // català inicial
 };
 
 // Afegim un event listener per canviar l'idioma quan l'usuari selecciona un idioma diferent
@@ -15,7 +15,6 @@ seleccionaIdioma.addEventListener("change", function () {
   const idiomaSeleccionat = this.value;
   canviarIdioma(idiomaSeleccionat);
 });
-
 
 //-------------------------------------------------
 // --------------- FUNCIONS -----------------------
@@ -35,33 +34,18 @@ async function carregaJSON() {
 
 // Funció per canviar l'idioma de la pàgina
 function canviarIdioma(idioma) {
-  
-  console.log(dadesIdiomes);
-  let trobat=false;
-  let index=-1;
+  // Trobar el número de registre de l'idioma
+  const index = dadesIdiomes.idiomes.findIndex(
+    (item) => item.idioma === idioma,
+  );
 
-  console.log(trobat);
-  console.log(index);
-  console.log(dadesIdiomes.idiomes.length);
-  // while(index<=dadesIdiomes.idiomes.length){
-  //   if(dadesIdiomes.idiomes.idioma===idioma){trobat=true;}
-  //   index++;
-  // console.log("hola");
-  // }
-  while(index<dadesIdiomes.idiomes.length){
-    console.log(dadesIdiomes.idiomes[index]);
-    index++;
-  }
+  document.getElementById("titol").innerHTML = dadesIdiomes.idiomes[index].titol; // idioma títol
 
-  // if (!dades) return;
-
-  // document.getElementById("titol").innerHTML = dades.titol; // idioma títol
-
-// Canvia idioma d'opcions de menú
+  // Canvia idioma d'opcions de menú
   const menu = document.querySelectorAll("li");
-  menu.forEach((element, index) => {
-    // element.innerHTML = dades.menu[index];
+  menu.forEach((element, posicio) => {
+    element.innerHTML = dadesIdiomes.idiomes[index].menu[posicio];
   });
 
-  // document.getElementById("peu").innerHTML = dades.peu; // idioma peu
+  document.getElementById("peu").innerHTML = dadesIdiomes.idiomes[index].peu; // idioma peu
 }
--- js/61 Pokemon.js
+++ js/61 Pokemon.js
@@ -1,53 +1,92 @@
+const API = "https://pokeapi.co/api/v2";
+const rutaFoto =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 const cataleg = document.getElementById("principal");
 
+const PAGE_SIZE = 12; // ajusta al teu límit
+let offset = 0; // ajusta si ja tens una variable pròpia
+
+const btnPrev = document.getElementById("previ");
+const btnSeg = document.getElementById("seguent");
+
+btnPrev.addEventListener("click", () => {
+  if (offset > 0) {
+    offset = Math.max(0, offset - PAGE_SIZE);
+    carregaDades(offset); // crida la teva funció que ja carrega el llistat
+  }
+});
+
+btnSeg.addEventListener("click", () => {
+  offset += PAGE_SIZE;
+  carregaDades(offset);
+});
+
 window.onload = async function () {
-  await carregaDades();
-  
+  await carregaDades(offset);
 };
 
 // Versió amb async/await
-async function carregaDades() {
+async function carregaDades(offset) {
+  document.getElementById("pagina").innerText="Pàgina "+offset%11;
   try {
-    const resposta = await fetch("https://pokeapi.co/api/v2/ability/");
+    const resposta = await fetch(
+      API + "/pokemon?limit=" + PAGE_SIZE + "&offset=" + offset,
+    );
     const pokemons = await resposta.json();
-    console.log(pokemons);
+    console.log(pokemons.results);
+    // Eliminem tots els fills del catàleg
+    cataleg.replaceChildren();
     //recorrer el camps per a la capçalera
-    pokemons.results.forEach(element => {
-        cataleg.appendChild(creaElement(element));
+    pokemons.results.forEach((element) => {
+      cataleg.appendChild(creaCarta(element.name, element.url, "hola",element.url));
     });
   } catch (e) {
     console.error("Error de càrrega ", e);
   }
 }
 
-function creaElement(element, etiqueta = "div") {
-    const camp = document.createElement(etiqueta);
-    camp.innerText = element.name;
+function creaElement(
+  etiqueta = "div",
+  classes = "",
+  text = "",
+  rutaImatge = ""
+) {
+  const patro = /\/(\d+)\/$/;
+  let numero = text.match(patro);
+  
+  const camp = document.createElement(etiqueta);
+  // Afegir classes
+  if (classes) {
+    let llistaClasses = classes.split(" ");
+    for (cl of llistaClasses) {
+      camp.classList.add(cl);
+    }
+  }
+  //  Afegir text
+  if (text) {
+    camp.textContent = text;
+  }
+  if (rutaImatge) {
+    camp.src = rutaFoto+numero[1]+".png";
+  }
+
   return camp;
 }
 
 // crea carta
-const carta=document.createElement("div");
-carta.classList.add("card");
-carta.classList.add("border-primary");
-carta.classList.add("mb-3");
-carta.style.maxWidth="18rem";
-// afegim elements de la carta
-const titolCarta=document.createElement("div");
-titolCarta.innerHTML="Sóc el titol de la carta";
-titolCarta.classList.add("card-header")
-carta.appendChild(titolCarta);
-// ageim titol del cos
-// afegim el body de la carta
-const cosCarta=document.createElement("div");
-const titolCos=document.createElement("h5");
-titolCos.classList.add("card-title");
-titolCos.innerText="Primary card title";
-cosCarta.appendChild(titolCos);
+function creaCarta(titolCapçalera, titol, text, url) {
+  const carta = creaElement("div", "card border-primary mb-3");
+  carta.style.maxWidth = "18rem";
 
-cosCarta.classList.add("card-body");
-cosCarta.classList.add("text-primary");
-carta.appendChild(cosCarta);
+  const header = creaElement("div", "card-header", titolCapçalera);
+  const foto = creaElement("img", "card-img-top",titol,url);
+  const body = creaElement("div", "card-body text-primary");
+  const titolBody = creaElement("h5", "card-title", titol);
+  const textBody = creaElement("p", "card-text", text);
 
+  body.append(titolBody, textBody);
+  carta.append(header, foto);
+  carta.append(header, body);
 
-cataleg.appendChild(carta);
+  return carta;
+}
