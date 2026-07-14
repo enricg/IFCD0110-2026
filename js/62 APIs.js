92348eb29fa8520e3145f55a82b8e7da0125963d
Enric <enricg@protonmail.com>
2026-07-14 09:00:34 +0200

    Canvi aparença pàgina APIS

--- js/62 APIs.js
+++ js/62 APIs.js
@@ -1,21 +1,45 @@
-const divUsuariRandom = document.getElementById("randomUser");
+const areaExercicis = document.getElementById("principal");
 const APIUsuariRandom = "https://randomuser.me/api/";
 
 window.onload = async function () {
-  await carregaDades(APIUsuariRandom);
+  // await carregaDades(APIUsuariRandom);
+  await areaExercicis.appendChild(
+    creaExercici(
+      1,
+      "Usuaris aleatoris",
+      "randomUser",
+      "API que crea usuaris aleatoris amb les seves característiques",
+      carregaDades,
+      esborraDades,
+    ),
+  );
 };
 
-// Versió amb async/await
-async function carregaDades(API) {
+// Carrega dades
+async function carregaDades() {
+  const divUsuariRandom = document.getElementById("solucio1");
+  const API = "https://randomuser.me/api/";
   try {
     const resposta = await fetch(API);
     const dades = await resposta.json();
     console.log(dades.results[0]);
     // Aquí va el codi
-    divUsuariRandom.appendChild(creaCarta(dades.results[0].name.first,"títol","text",dades.results[0].picture.large));
+    divUsuariRandom.appendChild(
+      creaCarta(
+        dades.results[0].name.first,
+        "títol",
+        "text",
+        dades.results[0].picture.large,
+      ),
+    );
   } catch (e) {
     console.error("Error de càrrega ", e);
   }
+}
+
+async function esborraDades() {
+  const divUsuariRandom = document.getElementById("solucio1");
+  divUsuariRandom.replaceChildren();
 }
 
 function creaElement(
@@ -24,7 +48,6 @@ function creaElement(
   text = "",
   rutaImatge = "",
 ) {
-
   const camp = document.createElement(etiqueta);
   // Afegir classes
   if (classes) {
@@ -58,4 +81,49 @@ function creaCarta(titolCapçalera, titol, text, url) {
   carta.append(body);
 
   return carta;
+}
+
+function creaExercici(
+  numeroExercici,
+  titol,
+  nomAPI,
+  descripcio,
+  funcioCarrega,
+  funcioNeteja
+) {
+  const exercici = creaElement("div", "container text-center");
+  const fila = creaElement("div", "row");
+  const esq = creaElement("div", "col-4");
+  const dre = creaElement("div", "col-8");
+  dre.style.display="flex";
+  dre.style.flexWrap="wrap";
+  dre.id = "solucio" + numeroExercici;
+  const tarjeta = creaElement("div", "card");
+  const titolTarjeta = creaElement("h5", "card-header");
+  titolTarjeta.innerText = titol;
+  tarjeta.appendChild(titolTarjeta);
+  const cos = creaElement("div", "card-body");
+  tarjeta.appendChild(cos);
+  const titolCos = creaElement("h5", "card-title");
+  titolCos.innerText = nomAPI;
+  const textCos = creaElement("p", "card-text");
+  textCos.innerText = descripcio;
+  const botoCarrega = creaElement("button", "btn btn-primary");
+  botoCarrega.innerText = "Carrega";
+  botoCarrega.id = "btnEx" + numeroExercici + "C";
+  botoCarrega.addEventListener("click", funcioCarrega);
+  const botoNeteja = creaElement("button", "btn btn-primary");
+  botoNeteja.innerText = "Neteja";
+  botoNeteja.id = "btnEx" + numeroExercici + "N";
+  botoNeteja.addEventListener("click", funcioNeteja);
+  cos.appendChild(titolCos);
+  cos.appendChild(textCos);
+  cos.appendChild(botoCarrega);
+  cos.appendChild(botoNeteja);
+  esq.appendChild(tarjeta);
+  fila.appendChild(esq);
+  fila.appendChild(dre);
+  exercici.appendChild(fila);
+
+  return exercici;
 }
